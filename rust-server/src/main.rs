e88bfa172c95eccf56827922824306cfac214897
Yudai Hayashi <yudai@wantedly.com>
2024-11-06 12:12:00 +0000

    Response from server

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -1,51 +1,45 @@
-// use actix_web::{get, post, web, App, HttpServer, Responder};
-// use serde::{Deserialize, Serialize};
-// 
-// #[derive(Serialize, Deserialize)]
-// struct ResponseObj {
-//     message: String,
-//     status_code: i32,
-// }
-// 
-// #[get("/hello/{name}")]
-// async fn greet(name: web::Path<String>) -> impl Responder {
-//     format!("Hello {name}!")
-// }
-// 
-// #[get("/json")]
-// async fn json() -> impl Responder {
-//     web::Json(ResponseObj {
-//         message: "Hello, World!".to_string(),
-//         status_code: 200,
-//     })
-// }
-// 
-// #[post("/json")]
-// async fn json_post(item: web::Json<ResponseObj>) -> impl Responder {
-//     format!("{} {}", item.message, item.status_code)
-// }
-// 
-// #[actix_web::main]
-// async fn main() -> std::io::Result<()> {
-//     HttpServer::new(|| App::new().service(greet).service(json).service(json_post))
-//         .bind(("127.0.0.1", 8080))?
-//         .run()
-//         .await
-// }
+use actix_web::{get, post, web, App, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
+use tch::{no_grad, Device, IValue, Tensor};
 
-use tch::{Device, Tensor, no_grad, IValue};
+#[derive(Serialize, Deserialize)]
+struct ResponseObj {
+    message: String,
+    status_code: i32,
+}
 
+#[get("/ping")]
+async fn ping() -> impl Responder {
+    "Pong!"
+}
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[post("/json")]
+async fn json_post(item: web::Json<ResponseObj>) -> impl Responder {
+    let output_value = predict_by_torch_model().unwrap();
+    format!("{} {:?}", item.message, output_value)
+}
+
+fn predict_by_torch_model() -> Result<Vec<Vec<f64>>, Box<dyn std::error::Error>> {
     let model = tch::CModule::load_on_device("/app/src/python-model/src/model.pt", Device::Cpu)?;
     let input_tensor = Tensor::randn(&[1, 10], (tch::Kind::Float, Device::Cpu));
     let input_ivalue = IValue::Tensor(input_tensor);
     let output_tensor = no_grad(|| model.forward_is(&[input_ivalue])).unwrap();
 
-    println!("Output: ");
     match output_tensor {
-        IValue::Tensor(t) => t.print(),
-        _ => println!("Output is not a tensor: {:?}", output_tensor),
+        IValue::Tensor(t) => {
+            let values: Vec<Vec<f64>> = Vec::try_from(&t)?;
+            Ok(values)
+        },
+        _ => {
+            Err("Output is not a tensor".into())
+        }
     }
-    Ok(())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(ping).service(json_post))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
